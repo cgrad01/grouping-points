4abda496e2772283bf5347a918c58c34bdc9d682
cgrad01 <cgrad01@gmail.com>
2016-03-31 12:49:38 -0400

    get distances from center point

--- runner.py
+++ runner.py
@@ -1,4 +1,11 @@
 from points import Point
 from route import Route
 
-Point.instantiate(Point.parse_json("points.json"))
+points = Point.instantiate(Point.parse_json("points.json"))
+route = Route()
+
+distances = []
+for point in points:
+  distances.append(route.center_point.get_distance(point))
+distances.sort()
+print distances
