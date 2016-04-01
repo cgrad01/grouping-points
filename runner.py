d1dd828cbf9d11c2bc2c227cbefd2ba9bc51e043
cgrad01 <cgrad01@gmail.com>
2016-04-01 09:07:38 -0400

    adjust for empty groups

--- runner.py
+++ runner.py
@@ -1,9 +1,13 @@
 from point import Point
+from group import Group
 from grouper import Grouper
 
 test = Grouper(3, "points.json")
 test.pass_groups()
 test.assign_group_members()
+for group in test.groups:
+  print len(group.members)
+test.adjust()
 
 for group in test.groups:
   print len(group.members)
